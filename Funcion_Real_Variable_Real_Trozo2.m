257c73e8a36b23cfbc84c3dbd10aa0666deaac08
gusio11gk12 <91569176+gusio11gk12@users.noreply.github.com>
2021-10-28 23:07:09 -0500

    Update Función Real Trozos 2
    propuesta de cambio

--- Funcion_Real_Variable_Real_Trozo2.m
+++ Funcion_Real_Variable_Real_Trozo2.m
@@ -1,20 +1,26 @@
 % Octave Script
 % Title                      :Funcion_Real_Variable_Real_Trozo2
 % Description                :Script para resolver funciones reales de variables reales
-% Author                     :Gustavo García Barrera(NONE_NAME)
+% Author                     :Gustavo GarcÃ­a Barrera(NONE_NAME)
 % Date                       :20211028
 % Version                    :1     
 % Usage                      :Este equipo> Documentos >octavescript> Funcion_Real_Variable_Real_Trozo2
-%                            :Requiere aplicación octave para usar su línea de comandos
+%                            :Requiere aplicaciÃ³n octave para usar su lÃ­nea de comandos
 %                            :https://octaveintro.readthedocs.io/en/latest/index.html
 
+% Funcion Compuessta
 clear
-syms x
-% Funcion_Real_Variable_Real (funcion 2 a trozo)
-% Calcular sqtr(x).*0<=x>=1+.*2-x.*1<x<=2
-f= @(x) sqrt(x).*(0<=x<=1)+(2-x).*(1<x<=2);
-f=0
-f=1
-f=2
-fplot(@(x)(sqrt(x)).*(0<=x<=1)+(2-x).*(1<x<=2),[-5,5])
-grid on
+
+% definimos el plano cartesiano con axis([min_x, max_x, min_y, max_y]) 
+axis([ -1, 5, 0, 1]);
+
+% Declaracion de la funcion a plotear
+y = ...
+ @ (x) (sqrt(x)).*( (x >= 0) & (x <= 1) )  + ...
+ ( 2 - x  ).*( (x > 1) & (x <= 2) );
+
+
+
+% Linea que grÃ¡fica nuestra funcion compuesta  
+fplot(y, axis);
+grid on;
